603a6be95c59ebe246681403f1211b18034ac711
EmilyC <emily.calandrella@tufts.edu>
2019-12-18 10:57:42 -0500

    challenges and mpu script

---
+++ all_challenges/all_challenges.ino
@@ -0,0 +1,221 @@
+#include <MPU6050.h>
+#include <Servo.h>
+
+// ******************** Constants ********************************************
+
+// For challenge 3
+ static int i;
+ static int prev_err;
+ int p;
+ int d;
+ int gas;
+ int kp;
+ int kd;
+ int ki;
+ int thresh; // set this to sensor value on black path
+
+ *****************************************************************************
+
+
+
+
+// ***************** From robot example code  *******************************
+
+Servo servo;
+
+// Motor control pins : L298N H bridge
+const int enAPin = 6; // Left motor PWM speed control
+const int in1Pin = 7; // Left motor Direction 1
+const int in2Pin = 5; // Left motor Direction 2
+// I switched pins 4 and 2 to make the motor directions correspond to the test function
+const int in3Pin = 2; // Right motor Direction 1
+const int in4Pin = 4; // Right motor Direction 2
+const int enBPin = 3; // Right motor PWM speed control
+
+enum Motor {LEFT, RIGHT};
+#define NUM_ANGLES 7
+unsigned char sensorAngle[NUM_ANGLES] = { 60, 70, 80, 90, 100, 110, 120 };
+unsigned int distance[NUM_ANGLES];
+
+
+// Set motor speed: 255 full ahead, -255 full reverse , 0 stop
+void go( enum Motor m, int speed)
+{
+  digitalWrite(m == LEFT ? in1Pin : in3Pin , speed > 0 ? HIGH : LOW);
+  digitalWrite(m == LEFT ? in2Pin : in4Pin , speed <= 0 ? HIGH : LOW);
+  analogWrite(m == LEFT ? enAPin : enBPin, speed < 0 ? -speed : speed);
+}
+
+// Initial motor test :
+// left motor forward then back
+// right motor forward then back
+void testMotors ()
+{
+  static int speed[8] = { 128, 255, 128, 0 ,
+  -128, -255, -128, 0};
+  go(RIGHT, 0);
+  for (unsigned char i = 0 ; i < 8 ; i++)
+    go(LEFT, speed[i ]), delay (200);
+  for (unsigned char i = 0 ; i < 8 ; i++)
+    go(RIGHT, speed[i ]), delay (200);
+}
+
+// ********************************************************************
+
+
+
+
+// *********************** Challenge 1 ***********************************
+
+// For imu
+/* 
+MPU6050 mpu;
+mpu.initialize();
+int ax, ay, az
+*/
+
+// duration is in ms
+// freq is in Hz
+void driveSine(int freq, int duration) {
+  Serial.print("f = ");
+  Serial.println(freq);
+  int start = millis();
+  int t = millis() - start;
+  int mspeed = 0;
+  while(t < duration) {
+    Serial.print("t = ");
+    Serial.println(t);
+    mspeed = sin(2*PI*freq*t)*255*20;
+    if (mspeed > 255) {
+      mspeed = 255;
+    }
+    //analogWrite(enBPin,mspeed); // if you input a positive mspeed the motors won't spin...
+    //analogWrite(enAPin,mspeed); 
+    Serial.print("moter speed = ");
+    Serial.println(mspeed);
+    go(LEFT,-mspeed);
+    go(RIGHT,mspeed);
+    t = millis() - start; 
+  }
+  go(LEFT,0);
+  go(RIGHT,0);
+  Serial.println("switch direction");
+  start = millis();
+  t = millis() - start;
+  mspeed = 0;
+  while(t < duration) {
+    mspeed = sin(2*PI*freq*t)*255*20;
+    mspeed = sin(2*PI*freq*t)*255*20;
+    if (mspeed > 255) {
+      mspeed = 255;
+    }
+    go(LEFT,mspeed);
+    go(RIGHT,-mspeed);
+    t = millis() - start; 
+  }
+  go(LEFT,0);
+  go(RIGHT,0);
+  Serial.println("end of driveSine");
+}
+
+//**************************************************************************
+
+
+
+
+// ********************* Challenge 2 ************************************************
+
+void Chall_2(int angle, int desired_angle, int kp) {
+
+  //Determine error
+  change = angle-desired_angle;
+
+  //Write gas to motor pin.
+  gas = change*kp;
+
+ //Make sure it's between -255 and 255.
+ if gas > 255
+    gas = 255;
+ else if gas < -255
+    gas = -255;
+ end
+
+  //Turn left or right.
+  if gas > 0
+      go(RIGHT, abs(gas));
+      go(LEFT,0);
+  else
+      go(LEFT, abs(gas));
+      go(RIGHT,0);
+  end 
+}
+
+//******************************************************************************
+
+
+
+// ****************** Challenge 3*********
+void Chall_3(kp,kd,ki) {
+
+  p = level - thresh;
+  
+  d = p - prev_err;
+  prev_err = p;
+
+  i = i + p;
+  
+  if p  = 0
+     i = 0;
+  end
+
+ //100 as constant so that it will go straight when no error. (see left below)
+  gas = 100 + (d*kd) + (p*kp) + (i*ki);
+
+  if gas > 255
+    gas = 255;
+  end
+
+  //Because traveling counter-clockwise, we will only need to travel right
+  go(left, 100);
+  go(right, gas);
+}
+
+// ***************************************************
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  digitalWrite (trigPin, LOW);
+  pinMode(enAPin, OUTPUT);
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(in3Pin, OUTPUT);
+  pinMode(in4Pin, OUTPUT);
+  pinMode(enBPin, OUTPUT);
+  servo.attach(servoPin);
+  servo.write(90);
+  go(LEFT, 0);
+  go(RIGHT, 0);
+  testMotors();
+
+  // for challenge 3
+  gas = 0;
+  i = 0;
+  prev_err = 0;
+
+
+
+}
+
+
+void loop() {
+  delay(2000);
+  Serial.println("entered loop");
+  driveSine(20,15000);
+  delay(2000);
+  Chall_3(5,.5,.5);
+
+}
---
+++ mpu/mpu.ino
@@ -0,0 +1,72 @@
+#include <MPU6050.h>
+
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int axVals[1000];
+int ayVals[1000];
+int azVals[1000];
+int gxVals[1000];
+int gyVals[1000];
+int gzVals[100];
+int count;
+int yaw_int;
+int yaw = 0;
+int time_step;
+
+
+#define OUTPUT_READABLE_ACCELGYRO
+
+
+void setup() {
+   Serial.begin(9600);
+   mpu.initialize();
+   Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+   count = 0;
+}
+
+void loop() {
+      // read raw accel/gyro measurements from device
+      mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+      yaw_int = gz;
+      count = 0;
+     
+  while(count< 1000) {
+    delay(0.1);
+    mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    yaw += (0.1*gz/131.2);
+    Serial.println(yaw);
+    // these methods (and a few others) are also available
+    //mpu.getAcceleration(&ax, &ay, &az);
+    //mpu.getRotation(&gx, &gy, &gz);
+
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+      /*
+        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.println(gz);
+    */
+    #endif
+/*
+    axVals[count] = ax;
+    ayVals[count] = ay;
+    azVals[count] = az;
+    gxVals[count] = gx;
+    gyVals[count] = gy; */
+    //gzVals[count] = gz;
+    count = count + 1;
+    //Serial.println("yaw = ");
+    //Serial.println(yaw);
+    
+    
+    }
+    //Serial.println("gzVals = ");
+    //Serial.println(gzVals[10]);
+
+    
+}
