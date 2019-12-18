cbca46dc4aefa239cc45e6a10d3bc20b7d1de382
EmilyC <emily.calandrella@tufts.edu>
2019-12-18 12:06:13 -0500

    test txt file

--- test_processing/test_processing.ino
+++ test_processing/test_processing.ino
@@ -1,16 +1,19 @@
+
 int x;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   x = 0;
-
 }
 
 void loop() {
   Serial.print(x);
-  Serial.print(",");
+  Serial.println(",");
   delay(500);
+  out.write(x);
   x++;
+  
+  
 
 }
